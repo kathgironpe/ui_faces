0145af21bc77999054ccfbb53074a49bc117800b
Katherine G. Pe <katherinepe@me.com>
2016-07-15 19:35:35 +0800

    Returns an authorized user feature

--- lib/ui_faces/face.ex
+++ lib/ui_faces/face.ex
@@ -2,13 +2,20 @@ defmodule UiFaces.Face do
   alias UiFaces.API
 
   @doc """
-  Return a random authorized user
+  Returns a random authorized user
 
   UiFaces.Face.random
   """
-
   def random do
     API.get("random").body
   end
 
+  @doc """
+  Returns an authorized user
+
+  UiFaces.Face.user("calebogden")
+  """
+  def user(username) do
+    API.get("user/#{username}").body
+  end
 end
